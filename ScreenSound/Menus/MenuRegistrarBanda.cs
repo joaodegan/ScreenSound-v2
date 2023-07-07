07a207373ba8ceb8f8135425a7644a456c16e2d8
degan <degan@DESKTOP-PFH6UMD>
2023-07-06 22:55:45 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -0,0 +1,31 @@
+﻿using OpenAI_API;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuRegistrarBanda : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        base.Executar(bandasRegistradas);
+        ExibirTituloDaOpcao("Registro das bandas");
+        Console.Write("Digite o nome da banda que deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+        Banda banda = new Banda(nomeDaBanda);
+        bandasRegistradas.Add(nomeDaBanda, banda);
+
+        var key = ""; // gerar chave na plataforma da OpenAi
+        var client = new OpenAIAPI(key);
+
+        var chat = client.Chat.CreateConversation();
+        chat.AppendSystemMessage($"Resumar a banda {nomeDaBanda} em um parágrafo");
+
+        string resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+        banda.Resumo = resposta;
+
+        Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!"); Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
